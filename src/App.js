a3671294975298585b87cc1da55e29d981d51cf2
matteoaricci <matteoaricci@DESKTOP-KID7KUE.localdomain>
2020-04-06 15:44:27 -0400

    Only need sort and filter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import MainContainer from './containers/MainContainer'
+const URL = 'http://localhost:3000/stocks'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      stocks: [],
+      portfolio: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(stock => this.setState({
+      stocks: stock
+    }))
+  }
+
+  purchaseStock = (stock) => {
+    this.state.portfolio.includes(stock) ? null : this.setState({portfolio: [...this.state.portfolio, stock]})
+  }
+
+  removeStock = (stock) => {
+    this.setState({
+      portfolio: this.state.portfolio.filter(port => port !== stock)
+    })
+  }
+
   render() {
     return (
       <div>
         <Header/>
-        <MainContainer/>
+        <MainContainer portfolio={this.state.portfolio} stocks={this.state.stocks} purchaseStock={this.purchaseStock} removeStock={this.removeStock}/>
       </div>
     );
   }
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -13,12 +13,12 @@ class MainContainer extends Component {
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.props.stocks} purchaseStock={this.props.purchaseStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer removeStock={this.props.removeStock} portfolio={this.props.portfolio}/>
 
             </div>
           </div>
